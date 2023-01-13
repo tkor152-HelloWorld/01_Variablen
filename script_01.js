fb2c8eb3d4c8bb500067a74d82835f0de1984fa1
Peter <schmidt.multimedia@gmail.com>
2023-01-13 14:32:02 +0100

    JS / untyped

--- script_01.js
+++ script_01.js
@@ -14,15 +14,23 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
 
-let familyName = "Mütze";
-console.log(familyName);
-console.log(firstName + " " + familyName);
+// let familyName = "Mütze";
+// console.log(familyName);
+// console.log(firstName + " " + familyName);
 
-// Variable test
+/* JS ist eine untypisierte Sprache! | untyped */
+
+let test; 
+test = "hallo";
+test = 2;
+test = true;
+
+console.log("Datenyp: " + typeof test);
+console.log("Inhalt: " + test);
 
 
 
