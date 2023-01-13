9ca9d3cc2272e645ee57c64042b04247a58fe67c
Peter <schmidt.multimedia@gmail.com>
2023-01-13 12:58:39 +0100

    02 Deklaration I

--- script_01.js
+++ script_01.js
@@ -14,65 +14,13 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-// let firstName; // Deklaration (Definition)
-// firstName = "Max"; // Wertzuweisung
-// console.log(firstName); // Ausgabe
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
 
-// let familyName = "Mütze"; // Deklaration + Wertzuweisung
-// console.log(familyName); // Ausgabe
-// console.log(firstName + " " + familyName);
-
-/***** 03 Deklaration + Wertzuweisung II *******/
-
-// let firstName, familyName;
-// firstName = prompt("Bitte Vornamen eingeben:"); 
-// familyName = prompt("Bitte Namen eingeben:"); 
-// console.log(firstName + " " + familyName);
-
-/* JS ist eine untypisierte Sprache! | untyped */
-
-// let test;
-// test = "Hi";
-// test = 2;
-// test = true;
-
-// console.log("Datentyp: " + typeof test);
-// console.log("Inhalt: " + test); // Ausgabe
-
-/***** 03a Konstanten *******/
-
-// const test = "Hallo"; // Variable mit KONSTANTEM Inhalt
-// test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
-// console.log("Inhalt: " + test); // Ausgabe
-
-/***** 04 Beispiel - Berechnung *******/
-
-// Deklaration
-// let ageJohn, ageMark;
-// let birthYearJohn, birthYearMark;
-
-// // Wertzuweisung
-// ageJohn = 25;
-// ageMark = 30;
-
-// let date = new Date();
-// console.log(date);
-// // let year = date.getFullYear();
-// let year = new Date().getFullYear();
-
-// // Berechnung
-// birthYearJohn = year - ageJohn;
-// birthYearMark = year - ageMark;
-
-// // Ausgabe
-// console.log("birthYearJohn: " + birthYearJohn);
-// console.log("birthYearMark: " + birthYearMark);
-
-
-
-
-
-
+let familyName = "Mütze";
+console.log(familyName);
+console.log(firstName + " " + familyName);
 
 
 
