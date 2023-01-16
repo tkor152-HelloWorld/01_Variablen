ea1db4eaf5d7e8f38b5579d4dc6d4e26e17690d1
Peter <schmidt.multimedia@gmail.com>
2023-01-16 09:32:01 +0100

    04 Beispiel - Berechnung I

--- script_01.js
+++ script_01.js
@@ -24,12 +24,12 @@
 
 /***** 03 Deklaration + Wertzuweisung II *******/
 
-let firstName, familyName;
-firstName = prompt("Bitte Vornamen eingeben."); // Wertzuweisung
-familyName = prompt("Bitte Nachnamen eingeben."); // Wertzuweisung
-console.log(firstName + " " + familyName);
+// let firstName, familyName;
+// firstName = prompt("Bitte Vornamen eingeben."); // Wertzuweisung
+// familyName = prompt("Bitte Nachnamen eingeben."); // Wertzuweisung
+// console.log(firstName + " " + familyName);
 
-console.log("Datenyp: " + typeof firstName);
+// console.log("Datenyp: " + typeof firstName);
 
 /* JS ist eine untypisierte Sprache! | untyped */
 
@@ -44,6 +44,22 @@ console.log("Datenyp: " + typeof firstName);
 /***** 03a Konstanten *******/
 
 // const test = "hallo";  // Variable mit KONSTANTEM Inhalt
-// //test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
+// // test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
 // console.log("Inhalt: " + test); // Ausgabe
+
+/***** 04 Beispiel - Berechnung *******/
+
+// Deklaration
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+let ageJohn, ageMark;
+
+let year = 2023;
+
+// Berechnung
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
+console.log(ageJohn);
+console.log(ageMark);
 
