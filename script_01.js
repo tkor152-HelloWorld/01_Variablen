8d22e2a1c2f1082e83d172c8fc20b64c90d036d7
Peter <schmidt.multimedia@gmail.com>
2023-01-16 09:15:33 +0100

    03 Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -22,6 +22,15 @@
 // console.log(familyName);
 // console.log(firstName + " " + familyName);
 
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+let firstName, familyName;
+firstName = prompt("Bitte Vornamen eingeben."); // Wertzuweisung
+familyName = prompt("Bitte Nachnamen eingeben."); // Wertzuweisung
+console.log(firstName + " " + familyName);
+
+console.log("Datenyp: " + typeof firstName);
+
 /* JS ist eine untypisierte Sprache! | untyped */
 
 // let test; 
@@ -34,7 +43,7 @@
 
 /***** 03a Konstanten *******/
 
-const test = "hallo";  // Variable mit KONSTANTEM Inhalt
-test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
-console.log("Inhalt: " + test); // Ausgabe
+// const test = "hallo";  // Variable mit KONSTANTEM Inhalt
+// //test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
+// console.log("Inhalt: " + test); // Ausgabe
 
